3afffd31ec0e6420722095984a8b7c2ea415bfec
akanumakonda <akanumakonda@NN121-HA-20.HS.sd203.org>
2015-10-15 10:29:34 -0500

    Epsilon

---
+++ Epsilon/String.java
@@ -0,0 +1,56 @@
+
+
+/**
+ * Write a description of class String here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class String
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private String word1;
+    private String word2;
+
+    /**
+     * Default constructor for objects of class String
+     */
+    public String()
+    {
+        // initialise instance variables
+        String word1 = "catalog";
+        String word2 = "cat";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public void main(String[] args)
+    {
+        // put your code here
+        int result = name1.compareTo("aaa");
+        System.out.println(result);
+        if (name1.equals(name2)){  
+            System.out.println("True");
+        }
+        else 
+        {
+            System.out.println("False");
+        }
+        if (word1.compareTo(word2) < 0){
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("");
+        
+            
+
